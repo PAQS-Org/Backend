be27c66becd99b14658c7f597414c69759063b9a
Aklowa <aklowadrumz@gmail.com>
2024-04-18 23:54:26 +0000

    tried to improve on payment model

--- payments/urls.py
+++ payments/urls.py
@@ -1,12 +1,7 @@
 from django.urls import path
-from .views import  (
-                    # initiate_payment, 
-                    verify_payment, 
-                    InitiatePayment
-                    )
+from .views import  (verify_payment, InitiatePayment)
 
 urlpatterns = [
-    # path("initiate-payment/", initiate_payment, name="initiate_payment"),
     path("initiate-payment/", InitiatePayment.as_view(), name='initiate-payment'),
     path("verify-payment/", verify_payment, name="verify_payment"),
 ]
--- payments/views.py
+++ payments/views.py
@@ -106,61 +106,6 @@ def verify_payment(request):
 
 
 
-
-
-# @csrf_exempt
-# def verify_payment(request):
-#     # retrive the payload from the request body
-#     payload = request.body
-#     # signature header to to verify the request is from paystack
-#     sig_header = request.headers['x-paystack-signature']
-#     body = None
-#     event = None
-
-#     try:
-#         # sign the payload with `HMAC SHA512`
-#         hash = hmac.new(PAYSTACK_SECRET_KEY.encode('utf-8'), payload, digestmod=hashlib.sha512).hexdigest()
-#         # compare our signature with paystacks signature
-#         if hash == sig_header:
-#             # if signature matches, 
-#             # proceed to retrive event status from payload
-#             body_unicode = payload.decode('utf-8')
-#             body = json.loads(body_unicode)
-#             # event status
-#             event = body['event']
-#         else:
-#             raise Exception
-#     except ValueError as e:
-#         # Invalid payload
-#         return HttpResponse(status=400)
-#     except KeyError as e:
-#         # Invalid payload
-#         return HttpResponse(status=400)
-#     except:
-#         # Invalid signature
-#         return HttpResponse(status=400)
-
-#     if event == 'charge.success':
-#         # if event status equals 'charge.success'
-#         # get the data and the `payment_id` 
-#         # we'd set in the metadata ealier
-#         data, transaction_id = body["data"], body['data']['metadata']['transaction_id']
-
-#         # validate status and gateway_response
-#         if (data["status"] == 'success') and (data["gateway_response"] == "Successful"):
-#             try:
-#                 payment = Payment.objects.get(id=transaction_id)
-#             except Payment.DoesNotExist:
-#                 return HttpResponse(status=404)
-#             # mark payment as paid
-#             payment.verified = True
-#             payment.save(force_update=True)
-
-#             print("PAID")
-
-#     return HttpResponse(status=200)
-
-
 def download_receipt(request, reference):
     try:
         transaction = Payment.objects.get(reference=reference)
