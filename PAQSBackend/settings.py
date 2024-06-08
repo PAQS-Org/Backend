835f4eed4fb5d74ba512e2c4891889e589ffebae
osahene <bra.kwameadu3@gmail.com>
2024-06-08 19:17:14 +0000

    restructured for railway

--- PAQSBackend/settings.py
+++ PAQSBackend/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ '127.0.0.1', 'localhost', '.vercel.app', '.now.sh']
+ALLOWED_HOSTS = [ '127.0.0.1', 'localhost']
 
 SITE_ID = 1
 
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -82,7 +83,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'PAQSBackend.wsgi.app'
+WSGI_APPLICATION = 'PAQSBackend.wsgi.application'
 
 
 # Database
@@ -146,7 +147,7 @@ STATICFILES_DIRS = [
         "static",
     ),
 ]
-STATICFILES_STORAGE = "whitenoise.django.GzipManifestStaticFilesStorage"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
