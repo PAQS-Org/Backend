55513679834209662504bd661d50ce9833ecd229
osahene <bra.kwameadu3@gmail.com>
2024-07-22 21:18:18 +0000

    trial

--- payments/views.py
+++ payments/views.py
@@ -23,8 +23,12 @@ from rest_framework.pagination import PageNumberPagination
 from weasyprint import HTML
 from django.template.loader import render_to_string
 from django.templatetags.static import static
+import sys
+import os
+sys.path.append('/usr/local/lib/weasyprint')
 
-
+print(sys.path.append('/usr/local/lib/weasyprint'))
+print(os.getcwd)
 class InitiatePayment(APIView):
     serializer_class = PaymentSerializer
     permission_classes = (IsAuthenticated, IsOwner)
