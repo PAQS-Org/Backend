81708cbca36771fd4ce0c2c19b7ed56aa29d413b
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2024-07-25 23:25:25 +0000

    group the generated qr codes per generation id

--- qrcodes/lib/generator.py
+++ qrcodes/lib/generator.py
@@ -1,27 +1,29 @@
 import qrcode
-from qrcode.image.pure import PyPNGImage
-import qrcode.image.svg
 import uuid
 import os
 
-def makeCode(gen_id:str, n: int, format: str) -> str:
+def makeCode(gen_id:str, n: int, format: str, path) -> str:
     app_url = "http://localhost"
     product = f"product{n:06}"
     code = f"{app_url}/{gen_id}/{product}"
     qr = qrcode.make(code)
-    filepath = f"qrcodes/data/{product}.{format}"
+    path = f"qrcodes/data/{gen_id}"
+    filepath = f"{path}/{product}.{format}"
     qr.save(filepath)
     return filepath
     
-def makeZip(qrCodes: list[str]) -> str:
+def makeZip(qrCodes: str) -> str:
     return ""
 
 def generate(count: int, format: str, logo: str) -> str:
     gen_id = str(uuid.uuid4())
-    qrCodes = []
+    path = f"qrcodes/data/{gen_id}"
+    os.mkdir(path)
+    
     for n in range(count):
-        qrCodes.append(makeCode(gen_id, n+1, format))
-    zipFilePath = makeZip(qrCodes)
+        makeCode(gen_id, n+1, format, path)
+        
+    zipFilePath = makeZip(path)
     return zipFilePath
 
 if __name__ == "__main__":
