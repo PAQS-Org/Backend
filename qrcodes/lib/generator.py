d7381b70966bc34fcc4d5999a2322030389f73ad
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2024-07-26 23:52:56 +0000

    refactor  generator

--- qrcodes/lib/generator.py
+++ qrcodes/lib/generator.py
@@ -3,11 +3,12 @@ import uuid
 import os
 import shutil
 
-def makeCode(gen_id:str, n: int, format: str, path) -> str:
+def makeImage(gen_id:str, n: int, format: str, path:str, logo: str | None = None) -> str:
     app_url = "http://localhost"
     product = f"product{n:06}"
     code = f"{app_url}/{gen_id}/{product}"
     qr = qrcode.make(code)
+    qrcode.make()
     path = f"qrcodes/data/{gen_id}"
     filepath = f"{path}/{product}.{format}"
     qr.save(filepath)
@@ -17,17 +18,21 @@ def makeZip(path: str, gen_id:str) -> str:
     zipPath = shutil.make_archive(base_name=path, format="zip", root_dir=path)
     return zipPath
 
-def generate(count: int, format: str, logo: str) -> str:
+def generate(count: int, format: str, logo: str | None = None) -> str:
+    if not os.path.exists("qrcodes/data"):
+        os.mkdir("qrcodes/data")
+    
     gen_id = str(uuid.uuid4())
-    print(gen_id)
     path = f"qrcodes/data/{gen_id}"
+    
     os.mkdir(path)
     
     for n in range(count):
-        makeCode(gen_id, n+1, format, path)
+        makeImage(gen_id, n+1, format, path, logo)
         
     zipFilePath = makeZip(path, gen_id)
     return zipFilePath
 
 if __name__ == "__main__":
-    generate(3, "png", "")
+    result = generate(3, "jpg", "")
+    print(result)
