85af67da9b1656e51ab8601b211a3e3eeb60d672
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2024-07-26 00:04:35 +0000

    zip the qrcodes

--- qrcodes/lib/generator.py
+++ qrcodes/lib/generator.py
@@ -1,6 +1,7 @@
 import qrcode
 import uuid
 import os
+import shutil
 
 def makeCode(gen_id:str, n: int, format: str, path) -> str:
     app_url = "http://localhost"
@@ -12,18 +13,20 @@ def makeCode(gen_id:str, n: int, format: str, path) -> str:
     qr.save(filepath)
     return filepath
     
-def makeZip(qrCodes: str) -> str:
-    return ""
+def makeZip(path: str, gen_id:str) -> str:
+    zipPath = shutil.make_archive(base_name=path, format="zip", root_dir=path)
+    return zipPath
 
 def generate(count: int, format: str, logo: str) -> str:
     gen_id = str(uuid.uuid4())
+    print(gen_id)
     path = f"qrcodes/data/{gen_id}"
     os.mkdir(path)
     
     for n in range(count):
         makeCode(gen_id, n+1, format, path)
         
-    zipFilePath = makeZip(path)
+    zipFilePath = makeZip(path, gen_id)
     return zipFilePath
 
 if __name__ == "__main__":
