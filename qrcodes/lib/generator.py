9773416457e08e4641e24f65c2838d3199eb52c5
osahene <bra.kwameadu3@gmail.com>
2024-08-08 15:02:48 +0000

    merged changes from qrcode branch

---
+++ qrcodes/lib/generator.py
@@ -0,0 +1,38 @@
+import qrcode
+import uuid
+import os
+import shutil
+
+def makeImage(gen_id:str, n: int, format: str, path:str, logo: str | None = None) -> str:
+    app_url = "http://localhost"
+    product = f"product{n:06}"
+    code = f"{app_url}/{gen_id}/{product}"
+    qr = qrcode.make(code)
+    qrcode.make()
+    path = f"qrcodes/data/{gen_id}"
+    filepath = f"{path}/{product}.{format}"
+    qr.save(filepath)
+    return filepath
+    
+def makeZip(path: str, gen_id:str) -> str:
+    zipPath = shutil.make_archive(base_name=path, format="zip", root_dir=path)
+    return zipPath
+
+def generate(count: int, format: str, logo: str | None = None) -> str:
+    if not os.path.exists("qrcodes/data"):
+        os.mkdir("qrcodes/data")
+    
+    gen_id = str(uuid.uuid4())
+    path = f"qrcodes/data/{gen_id}"
+    
+    os.mkdir(path)
+    
+    for n in range(count):
+        makeImage(gen_id, n+1, format, path, logo)
+        
+    zipFilePath = makeZip(path, gen_id)
+    return zipFilePath
+
+if __name__ == "__main__":
+    result = generate(3, "jpg", "")
+    print(result)
