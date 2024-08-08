e32479430e1e5374a8708b26caa5622e57d0e757
osahene <bra.kwameadu3@gmail.com>
2024-08-08 18:21:17 +0000

    tweaked the code so that each qr code generated gets a unique uuid code

--- qrcodes/lib/generator.py
+++ qrcodes/lib/generator.py
@@ -3,32 +3,31 @@ import uuid
 import os
 import shutil
 
-def makeImage(gen_id:str, n: int, format: str, path:str, logo: str | None = None) -> str:
+def makeImage(n: int, format: str, path: str, logo: str | None = None) -> str:
     app_url = "http://localhost"
+    gen_id = str(uuid.uuid4())  # Generate a new UUID for each QR code
     product = f"product{n:06}"
     code = f"{app_url}/{gen_id}/{product}"
     qr = qrcode.make(code)
-    qrcode.make()
-    path = f"qrcodes/data/{gen_id}"
     filepath = f"{path}/{product}.{format}"
     qr.save(filepath)
     return filepath
-    
-def makeZip(path: str, gen_id:str) -> str:
-    zipPath = shutil.make_archive(base_name=path, format="zip", root_dir=path)
+
+def makeZip(path: str, gen_id: str) -> str:
+    zipPath = shutil.make_archive(base_name=f"qrcodes/data/{gen_id}", format="zip", root_dir=path)
     return zipPath
 
 def generate(count: int, format: str, logo: str | None = None) -> str:
     if not os.path.exists("qrcodes/data"):
         os.mkdir("qrcodes/data")
     
-    gen_id = str(uuid.uuid4())
+    gen_id = str(uuid.uuid4()) 
     path = f"qrcodes/data/{gen_id}"
     
     os.mkdir(path)
     
     for n in range(count):
-        makeImage(gen_id, n+1, format, path, logo)
+        makeImage(n+1, format, path, logo)
         
     zipFilePath = makeZip(path, gen_id)
     return zipFilePath
