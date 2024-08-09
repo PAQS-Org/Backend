9c42c9a781e8deaa310bebf116d2190e177de2c6
osahene <bra.kwameadu3@gmail.com>
2024-08-09 10:42:27 +0000

    added staticfiles unconvensionally

--- PAQSBackend/settings.py
+++ PAQSBackend/settings.py
@@ -261,6 +261,8 @@ AWS_DEFAULT_ACL =  None
 AWS_S3_VERITY = True
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
+
+
 CONTENT_SECURITY_POLICY = {
     "EXCLUDE_URL_PREFIXES": ["/excluded-path/"],
     "DIRECTIVES": {
