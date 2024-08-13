707705604f326540bb4f5c5eb9c4bf56a33a3ce7
osahene <bra.kwameadu3@gmail.com>
2024-08-13 08:19:44 +0000

    embedded the logo on the qr code

--- payments/lib/generator.py
+++ payments/lib/generator.py
@@ -1,4 +1,5 @@
 import qrcode
+from PIL import Image, ImageDraw, ImageFont
 import uuid
 import os
 import shutil
@@ -13,11 +14,27 @@ def makeImage(n: int, format: str, path: str, comp: str, prod: str, batch: str, 
     batch_number = f"{batch}"
     code = f"{app_url}/{gen_id}/{company}/{product}"
     qr = qrcode.make(code)
+
+    # Add the logo in the middle of the QR code if provided
+    if logo:
+        logo_image = Image.open(logo)
+        logo_box = (qr.size[0] // 2 - logo_image.size[0] // 2, qr.size[1] // 2 - logo_image.size[1] // 2)
+        qr.paste(logo_image, logo_box)
+
+    # Add "Scan Me" text below the QR code
+    qr = qr.convert("RGBA")
+    draw = ImageDraw.Draw(qr)
+    font = ImageFont.load_default()  # You can use a custom font here
+    text = f"This is managed by PAQS for {comp}"
+    text_width, text_height = draw.textsize(text, font=font)
+    text_position = ((qr.size[0] - text_width) // 2, qr.size[1] - text_height - 10)  # Position it just above the bottom
+    draw.text(text_position, text, font=font, fill=(0, 0, 0))
+
+   # Save the QR code with the specified naming convention
     filepath = f"{path}/{company}_{product}_{batch_number}_{n}.{format}"  # Updated filepath format
     os.makedirs(os.path.dirname(filepath), exist_ok=True)  # Create directories if they don't exist
     qr.save(filepath)
     return gen_id, filepath
-
 
 def makeZip(path: str, comp: str, prod: str, batch: str, gen_id: str) -> str:
     zip_filename = f"{comp}_{prod}_{batch}_{gen_id}.zip"  # Updated zip file name format
