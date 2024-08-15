ca2863556e2a7909344939dacb35e44e0b4a0e6a
osahene <bra.kwameadu3@gmail.com>
2024-08-15 21:51:43 +0000

    .

--- PAQSBackend/celery.py
+++ PAQSBackend/celery.py
@@ -1,10 +1,11 @@
 import os
 
 from celery import Celery
+from django.conf import settings
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PAQSBackend.settings")
 
-app = Celery("PAQSBackend")
+app = Celery("PAQSBackend", broker=settings.CELERY_BROKER_URL)
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
