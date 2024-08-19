8d1f09bb742fd4951627a711889b19db768a1976
osahene <bra.kwameadu3@gmail.com>
2024-08-19 18:04:11 +0000

    prod url

--- product/urls.py
+++ product/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import ScanInfoView
 
 urlpatterns = [
-    path('qrinfo/', ScanInfoView.as_view, name='qr_info'),
+    path('qrinfo/', ScanInfoView.as_view(), name='qr_info'),
     # path('generate/', GenerateAndLogQR.as_view, name='generate'),
 ]
