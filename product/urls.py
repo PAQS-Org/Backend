cab2f74403a9f4a32d3dc37f546ceb3ce3dd732c
osahene <bra.kwameadu3@gmail.com>
2024-08-20 20:44:11 +0000

    added the urls

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ScanInfoView
+from .views import ScanInfoView, CheckoutInfoView
 
 urlpatterns = [
     path('qrinfo/', ScanInfoView.as_view(), name='qr_info'),
+    path('checkoutinfo/', CheckoutInfoView.as_view(), name='checkout_info'),
     # path('generate/', GenerateAndLogQR.as_view, name='generate'),
 ]
