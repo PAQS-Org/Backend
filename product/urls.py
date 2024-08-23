22098cb79bdf8e3c632899efcc2d9cbd2811d3a9
osahene <bra.kwameadu3@gmail.com>
2024-08-23 00:15:09 +0000

    added the header metrics

--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from .views import ScanInfoView, CheckoutInfoView, PatchInfoView
+from .views import (
+    ScanInfoView, CheckoutInfoView, PatchInfoView,
+    ScanMetricsView, CheckoutMetricsView, TopLocationMetrics,
+    PerformanceMetricsView
+    )
 
 urlpatterns = [
     path('qrinfo/', ScanInfoView.as_view(), name='qr_info'),
     path('checkoutinfo/', CheckoutInfoView.as_view(), name='checkout_info'),
     path('patchinfo/', PatchInfoView.as_view(), name='patch_info'),
+    path('scanmetrics/', ScanMetricsView.as_view(), name='scan_metrics'),
+    path('checkoutmetrics/', CheckoutMetricsView.as_view(), name='checkout_metrics'),
+    path('toplocation/', TopLocationMetrics.as_view(), name='top_location'),
+    path('performance/', PerformanceMetricsView.as_view(), name='performance_metrics'),
 ]
--- product/views.py
+++ product/views.py
@@ -1,9 +1,10 @@
+from django.shortcuts import get_object_or_404
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import LogProduct, ScanInfo
+from .models import LogProduct, ScanInfo, CheckoutInfo
 from rest_framework.permissions import IsAuthenticated
 from accounts.permissions import IsOwner, IsUser
 from .serializer import CheckoutInfoSerializer, ScanInfoSerializer, LogProductSerializer
@@ -12,6 +13,10 @@ from django.core.mail import send_mail, BadHeaderError
 from django.conf import settings
 import logging
 from smtplib import SMTPException
+from django.core.cache import cache
+from django.db.models import Count, Avg, F, Max
+from django.utils import timezone
+import datetime
 
 @method_decorator(csrf_exempt, name='dispatch')
 class ScanInfoView(APIView):
@@ -209,3 +214,199 @@ class PatchInfoView(APIView):
         except Exception as e:
             logging.error(f"Unexpected error occurred when sending email to {user_email}: {str(e)}")
 
+
+class ScanMetricsView(APIView):
+    permission_classes = [IsAuthenticated, IsOwner]
+
+    def get(self, request, *args, **kwargs):
+        company_name = request.data.company_name  # Assuming the user model has a company_name field
+        cache_key = f"scan_metrics_{company_name}"
+        data = cache.get(cache_key)
+
+        if not data:
+            # Filter data by company and exclude rows with empty fields
+            filtered_data = ScanInfo.objects.filter(
+                company_name=company_name
+            ).exclude(
+                country='',
+                region='',
+                city=''
+            )
+
+            # Total number of rows
+            total_rows = filtered_data.count()
+
+            # Sum of rows for the prevailing month
+            current_month = timezone.now().month
+            rows_current_month = filtered_data.filter(
+                date_time__month=current_month
+            ).count()
+
+            # Average data received from the beginning of the year to the previous day of the current day
+            current_year = timezone.now().year
+            yesterday = timezone.now().date() - datetime.timedelta(days=1)
+            average_data_ytd = filtered_data.filter(
+                date_time__year=current_year,
+                date_time__lt=yesterday
+            ).aggregate(avg=Avg('date_time'))['avg']
+
+            # Average data received per day
+            days_since_first_entry = (timezone.now().date() - filtered_data.earliest('date_time').date_time).days
+            average_per_day = total_rows / days_since_first_entry if days_since_first_entry > 0 else total_rows
+
+            data = {
+                "total_rows": total_rows,
+                "rows_current_month": rows_current_month,
+                "average_data_ytd": average_data_ytd,
+                "average_per_day": average_per_day
+            }
+
+            cache.set(cache_key, data, timeout=3600)  
+
+        return Response(data)
+    
+
+class CheckoutMetricsView(APIView):
+
+    permission_classes = [IsAuthenticated, IsOwner]
+
+    def get(self, request, *args, **kwargs):
+        company_name = request.data.company_name  # Assuming the user model has a company_name field
+        cache_key = f"checkout_metrics_{company_name}"
+        data = cache.get(cache_key)
+
+        if not data:
+            # Filter data by company and exclude rows with empty fields
+            filtered_data = CheckoutInfo.objects.filter(
+                company_name=company_name
+            ).exclude(
+                country='',
+                region='',
+                city=''
+            )
+
+            # Total number of rows
+            total_rows = filtered_data.count()
+
+            # Sum of rows for the prevailing month
+            current_month = timezone.now().month
+            rows_current_month = filtered_data.filter(
+                date_time__month=current_month
+            ).count()
+
+            # Average data received from the beginning of the year to the previous day of the current day
+            current_year = timezone.now().year
+            yesterday = timezone.now().date() - datetime.timedelta(days=1)
+            average_data_ytd = filtered_data.filter(
+                date_time__year=current_year,
+                date_time__lt=yesterday
+            ).aggregate(avg=Avg('date_time'))['avg']
+
+            days_since_first_entry = (timezone.now().date() - filtered_data.earliest('date_time').date_time).days
+            average_per_day = total_rows / days_since_first_entry if days_since_first_entry > 0 else total_rows
+
+            data = {
+                "total_rows": total_rows,
+                "rows_current_month": rows_current_month,
+                "average_data_ytd": average_data_ytd,
+                "average_per_day": average_per_day
+            }
+
+            cache.set(cache_key, data, timeout=3600)  
+
+        return Response(data)
+    
+
+class TopLocationMetrics(APIView):
+    permission_classes = [IsAuthenticated, IsOwner]
+
+    def get(self, request, *args, **kwargs):
+        company_name = request.data.company_name  # Assuming the user model has a company_name field
+        cache_key = f"metrics_comparison_{company_name}"
+        data = cache.get(cache_key)
+
+        if not data:
+            # Common function to get location with highest and lowest counts
+            def get_location_metrics(queryset, location_fields):
+                location_data = queryset.values(*location_fields).annotate(count=Count('id'))
+                highest_location = location_data.order_by('-count').first()
+                lowest_location = location_data.order_by('count').first()
+                return highest_location, lowest_location
+
+            # Locations to consider in the order
+            location_fields = ['country', 'region', 'city', 'town']
+
+            # ScanInfo Metrics
+            scan_queryset = ScanInfo.objects.filter(company_name=company_name).exclude(
+                country='',
+                region='',
+                city='',
+                town='',
+            )
+            highest_scan_location, lowest_scan_location = get_location_metrics(scan_queryset, location_fields)
+
+            # CheckoutInfo Metrics
+            checkout_queryset = CheckoutInfo.objects.filter(company_name=company_name).exclude(
+                country='',
+                region='',
+                city='',
+                town='',
+            )
+            highest_checkout_location, lowest_checkout_location = get_location_metrics(checkout_queryset, location_fields)
+
+            # Product with the highest checkout
+            highest_checkout_product = checkout_queryset.values('product_name').annotate(total=Count('id')).order_by('-total').first()
+
+            data = {
+                "highest_scan_location": highest_scan_location,
+                "lowest_scan_location": lowest_scan_location,
+                "highest_checkout_location": highest_checkout_location,
+                "lowest_checkout_location": lowest_checkout_location,
+                "highest_checkout_product": highest_checkout_product
+            }
+
+            # Cache the results
+            cache.set(cache_key, data, timeout=3600)  # Cache for 1 hour
+
+        return Response(data)
+    
+
+class PerformanceMetricsView(APIView):
+    permission_classes = [IsAuthenticated, IsOwner]
+
+    def get(self, request, *args, **kwargs):
+        company_name = request.data.company_name  # Assuming the user model has a company_name field
+        cache_key = f"performance_metrics_{company_name}"
+        data = cache.get(cache_key)
+
+        if not data:
+            # 1. Conversion rate calculation
+            total_scans = ScanInfo.objects.filter(company_name=company_name).count()
+            total_checkouts = CheckoutInfo.objects.filter(company_name=company_name).count()
+            conversion_rate = (total_checkouts / total_scans) * 100 if total_scans > 0 else 0
+
+            # 2. Month and year with the highest checkout
+            checkout_by_month = CheckoutInfo.objects.filter(company_name=company_name).annotate(
+                year=F('date_time__year'),
+                month=F('date_time__month')
+            ).values('year', 'month').annotate(
+                checkout_count=Count('id')
+            ).order_by('-checkout_count').first()
+
+            if checkout_by_month:
+                highest_checkout_month = f"{checkout_by_month['month']}, {checkout_by_month['year']}"
+                highest_checkout_month_value = checkout_by_month['checkout_count']
+            else:
+                highest_checkout_month = None
+                highest_checkout_month_value = 0
+
+            data = {
+                "conversion_rate": conversion_rate,
+                "highest_checkout_month": highest_checkout_month,
+                "highest_checkout_month_value": highest_checkout_month_value,
+            }
+
+            # Cache the results
+            cache.set(cache_key, data, timeout=3600)  # Cache for 1 hour
+
+        return Response(data)
