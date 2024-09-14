ef663622175ca4e60d1ad337328f8d2321f65649
osahene <bra.kwameadu3@gmail.com>
2024-09-14 23:59:49 +0000

    rm django-mongoengine

--- product/admin.py
+++ product/admin.py
@@ -1,21 +1,21 @@
-from django_mongoengine.mongo_admin import DocumentAdmin
+# from django_mongoengine.mongo_admin import DocumentAdmin
 from django.contrib import admin
 from .models import ScanInfo, CheckoutInfo, LogProduct
 # Register your models here.
 
-class ScanInfoAdmin(DocumentAdmin):
-    list_display = ('company_name', 'product_name', 'batch_number', 'user_name', 'date_time')
-    search_fields = ('company_name', 'product_name', 'batch_number')
+# class ScanInfoAdmin(DocumentAdmin):
+#     list_display = ('company_name', 'product_name', 'batch_number', 'user_name', 'date_time')
+#     search_fields = ('company_name', 'product_name', 'batch_number')
 
-class CheckoutInfoAdmin(DocumentAdmin):
-    list_display = ('company_name', 'product_name', 'batch_number', 'user_name', 'date_time')
-    search_fields = ('company_name', 'product_name', 'batch_number')
+# class CheckoutInfoAdmin(DocumentAdmin):
+#     list_display = ('company_name', 'product_name', 'batch_number', 'user_name', 'date_time')
+#     search_fields = ('company_name', 'product_name', 'batch_number')
 
-class LogProductInfoAdmin(DocumentAdmin):
-    list_display = ('company_name', 'product_name', 'batch_number', 'user_name', 'date_time')
-    search_fields = ('company_name', 'product_name', 'batch_number')
+# class LogProductInfoAdmin(DocumentAdmin):
+#     list_display = ('company_name', 'product_name', 'batch_number', 'user_name', 'date_time')
+#     search_fields = ('company_name', 'product_name', 'batch_number')
+# ScanInfoAdmin,CheckoutInfoAdmin,LogProductInfoAdmin
 
-
-admin.site.register(ScanInfo, ScanInfoAdmin)
-admin.site.register(CheckoutInfo, CheckoutInfoAdmin)
-admin.site.register(LogProduct, LogProductInfoAdmin)
+admin.site.register(ScanInfo)
+admin.site.register(CheckoutInfo)
+admin.site.register(LogProduct)
