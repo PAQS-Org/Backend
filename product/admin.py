2218e8a06755fa259d61b49693254aa12fc21269
osahene <bra.kwameadu3@gmail.com>
2024-09-15 02:33:44 +0000

    fixed the admin

--- product/admin.py
+++ product/admin.py
@@ -1,21 +1,8 @@
-# from django_mongoengine.mongo_admin import DocumentAdmin
-# from django.contrib import admin
-# from .models import ScanInfo, CheckoutInfo, LogProduct
+from django.contrib import admin
+from .models import ScanInfo, CheckoutInfo, LogProduct
 # Register your models here.
 
-# class ScanInfoAdmin(DocumentAdmin):
-#     list_display = ('company_name', 'product_name', 'batch_number', 'user_name', 'date_time')
-#     search_fields = ('company_name', 'product_name', 'batch_number')
 
-# class CheckoutInfoAdmin(DocumentAdmin):
-#     list_display = ('company_name', 'product_name', 'batch_number', 'user_name', 'date_time')
-#     search_fields = ('company_name', 'product_name', 'batch_number')
-
-# class LogProductInfoAdmin(DocumentAdmin):
-#     list_display = ('company_name', 'product_name', 'batch_number', 'user_name', 'date_time')
-#     search_fields = ('company_name', 'product_name', 'batch_number')
-# ScanInfoAdmin,CheckoutInfoAdmin,LogProductInfoAdmin
-
-# admin.site.register(ScanInfo)
-# admin.site.register(CheckoutInfo)
-# admin.site.register(LogProduct)
+admin.site.register(ScanInfo)
+admin.site.register(CheckoutInfo)
+admin.site.register(LogProduct)
